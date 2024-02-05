483b39c41c1db667c358ef1eb0b7e4ccad7b71b9
Hashan0217 <hashan@88.com>
2024-02-05 18:37:34 +0800

    添加阿里icon

--- KleaTodo_X-FrontWebSite/src/App.vue
+++ KleaTodo_X-FrontWebSite/src/App.vue
@@ -4,6 +4,9 @@
    <div>
      hello
    </div>
+   <div style="width: 100px;height: 100px;">
+      <i class="iconfont kelaIcot-daxiongmaojiancejilu" style="width: 100%;height: 100%;"></i>
+   </div>
 </template>
 
 
